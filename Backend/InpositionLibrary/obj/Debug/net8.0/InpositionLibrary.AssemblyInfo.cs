75f8357f116009654b3795728a85a6a643f847b8
Seladin Begu <beguseladin@gmail.com>
2024-05-28 17:06:07 +0200

    aa

--- Backend/InpositionLibrary/obj/Debug/net8.0/InpositionLibrary.AssemblyInfo.cs
+++ Backend/InpositionLibrary/obj/Debug/net8.0/InpositionLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InpositionLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2b18a3e8373c6fda37b73759dd8f9d9db44f0d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c40f91609550dcf3aa25c83b11a523432e13a70c")]
 [assembly: System.Reflection.AssemblyProductAttribute("InpositionLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InpositionLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
